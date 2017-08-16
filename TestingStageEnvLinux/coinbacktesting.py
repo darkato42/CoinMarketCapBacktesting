0fd03ff81df6eaf71a2f25453cc94a4dae840708
Yulin Wu <adamoweb@gmail.com>
2017-08-16 14:47:50 +0100

    add code for testing bt framework

--- TestingStageEnvLinux/coinbacktesting.py
+++ TestingStageEnvLinux/coinbacktesting.py
@@ -1,11 +1,17 @@
 import coinrepo
+import bt
 
 def main():
     '''entry point'''
     
+    # Test MyCoin DB connection
     symbol_list = ['BTC', 'ETH']
     result = coinrepo.get_coinhistory(symbol_list)
-    print result
+    print result.head()
     
+    # Test bt framework installed properly
+    data = bt.get('aapl,msft,c,gs,ge', start='2010-01-01')
+    print data.head()
+
 if __name__ == "__main__":
     main()
--- TestingStageEnvWin/coinbacktesting.py
+++ TestingStageEnvWin/coinbacktesting.py
@@ -1,11 +1,12 @@
 import coinrepo
+import bt
 
 def main():
     '''entry point'''
     
     symbol_list = ['BTC', 'ETH']
     result = coinrepo.get_coinhistory(symbol_list)
-    print result
+    print result.head()
     
 if __name__ == "__main__":
     main()
