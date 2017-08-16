ec5b010bc774f6f5e1ccb11d3dad144d3c7494d8
Yulin Wu <adamoweb@gmail.com>
2017-08-16 19:14:37 +0100

    add zipline test example

---
+++ TestingStageEnvLinux/coinbacktesting_bt.py
@@ -0,0 +1,39 @@
+import coinrepo
+import bt
+
+def btexample():
+    # fetch some data
+    data = bt.get('spy,agg', start='2010-01-01')
+    print data.head()
+
+    # create the strategy
+    s = bt.Strategy('s1', [bt.algos.RunMonthly(),
+                        bt.algos.SelectAll(),
+                        bt.algos.WeighEqually(),
+                        bt.algos.Rebalance()])
+
+    # create a backtest and run it
+    test = bt.Backtest(s, data)
+    res = bt.run(test)
+
+    # first let's see an equity curve
+    plot = res.plot()
+    fig = plot.get_figure()
+    fig.savefig("foo.png")
+
+def main():
+    '''entry point'''
+    
+    # # Test bt framework installed properly  
+    # data = bt.get('aapl,msft,c,gs,ge', start='2010-01-01')
+    # print data.head()
+
+    # # Test MyCoin DB connection
+    # symbol_list = ['BTC', 'ETH']
+    # result = coinrepo.get_coinhistory(symbol_list)
+    # print result.head()
+    
+    btexample()
+
+if __name__ == "__main__":
+    main()
