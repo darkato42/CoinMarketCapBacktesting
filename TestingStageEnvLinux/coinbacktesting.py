abf8ce097b4ba70d3122cc7caf98ed3604cc002a
Yulin Wu <adamoweb@gmail.com>
2017-08-16 15:57:32 +0100

    Test data visualization.

--- TestingStageEnvLinux/coinbacktesting.py
+++ TestingStageEnvLinux/coinbacktesting.py
@@ -1,18 +1,7 @@
 import coinrepo
 import bt
 
-def main():
-    '''entry point'''
-    
-    # # Test bt framework installed properly  
-    # data = bt.get('aapl,msft,c,gs,ge', start='2010-01-01')
-    # print data.head()
-
-    # # Test MyCoin DB connection
-    # symbol_list = ['BTC', 'ETH']
-    # result = coinrepo.get_coinhistory(symbol_list)
-    # print result.head()
-    
+def btexample():
     # fetch some data
     data = bt.get('spy,agg', start='2010-01-01')
     print data.head()
@@ -28,7 +17,23 @@ def main():
     res = bt.run(test)
 
     # first let's see an equity curve
-    res.plot()
+    plot = res.plot()
+    fig = plot.get_figure()
+    fig.savefig("foo.png")
+
+def main():
+    '''entry point'''
+    
+    # # Test bt framework installed properly  
+    # data = bt.get('aapl,msft,c,gs,ge', start='2010-01-01')
+    # print data.head()
+
+    # # Test MyCoin DB connection
+    # symbol_list = ['BTC', 'ETH']
+    # result = coinrepo.get_coinhistory(symbol_list)
+    # print result.head()
+    
+    btexample()
 
 if __name__ == "__main__":
     main()
--- TestingStageEnvWin/coinbacktesting.py
+++ TestingStageEnvWin/coinbacktesting.py
@@ -7,10 +7,11 @@ def main():
     symbol_list = ['BTC', 'ETH']
     result = coinrepo.get_coinhistory(symbol_list)
     plot = result.plot(figsize=(15,5))
-
-    # Test bt framework installed properly
-    #data = bt.get('aapl,msft,c,gs,ge', start='2010-01-01')
-    #print data.head()
+    fig = plot.get_figure()
+    fig.savefig("foo.png")
+    # # Test bt framework installed properly
+    # data = bt.get('aapl,msft,c,gs,ge', start='2010-01-01')
+    # print data.head()
 
 if __name__ == "__main__":
     main()
